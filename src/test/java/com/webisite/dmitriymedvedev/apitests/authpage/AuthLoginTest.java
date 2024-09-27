b0a350795238f25d93af90cbee3064f4fcb1e6fb
Dzmitryi Miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
2024-09-27 15:02:00 +0300

    add Loginrequest responses, refactoring AuthLoginTest class

--- src/test/java/com/webisite/dmitriymedvedev/apitests/authpage/AuthLoginTest.java
+++ src/test/java/com/webisite/dmitriymedvedev/apitests/authpage/AuthLoginTest.java
@@ -13,100 +13,87 @@ import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItems;
 
-    @Nested
-    public class AuthLoginTest {
+@Nested
+public class AuthLoginTest {
 
-        @Test
-        public void testVKLogin() {
-            when().
-                    post(LoginRequest.VK_LOGIN_URL).
-                    then().
-                    log().all().
-                    statusCode(200).
-                    body(LoginRequest.getVKLoginErrorInfo()[0], equalTo(LoginRequest.getVKLoginErrorInfo()[1]));
-        }
-
-        @Test
-        public void testUnLogin() {
-            Map<String,String> headers = new HashMap<>();
-            headers.put("Accept","application/json, text/plain, */*");
-            given().headers(headers).
-                    when().
-                    get("\n" +
-                            "https://passport.skillbox.ru/authapi/v1/user/").
-                    then().
-                    statusCode(401).
-                    body("message",equalTo("Unauthenticated."));
-
-        }
-
-        @Test
-        public void testUserInfo() {
-            String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5YTczZmEyYi0yOTk4LTRlNjEtYWU5YS02N2NmMzdhZmQyZWIiLCJqdGkiOiIyMGQ3OGZmNTVlZjRiOTczYmMyM2ExMDI0ZTMwZmU4NjQ1YjBjYzBmZWJkMWJiZTk2YjJjZmJmMjUyN2NkODUzNzMzNGMzNGI4NzY3MDI4NSIsImlhdCI6MTcyNzMyODE1Mi43Mzg2NDcsIm5iZiI6MTcyNzMyODE1Mi43Mzg2NDksImV4cCI6MTcyNzM3MTM1Mi43MjIxOTYsInN1YiI6IjQwMDk4MzciLCJyb2xlcyI6W10sInJvbGVzX2J5X2NvbXBhbmllcyI6e30sImlzX2Jsb2NrZWQiOmZhbHNlLCJzY29wZXMiOlsib3BlbmlkIiwiZW1haWwiLCJwcm9maWxlIiwicm9sZXMiLCJwZXJtaXNzaW9ucyJdfQ.Fa5Vjvz-bVMLvx0bajYajnD7R9L2i16iPl3_Kco-woMaRMkISfH6AunxQebf5BFQ5aYZDyFB0MxYCQwr0AemxoVTm_IH4bNi1Jo_dcjly0Td5QF16XvQYRyU84qQXYZb2mq3agHLHzhifDY3_xf0azIXI4uQKPjWItFMlDBYP8C-3Jx-iBq1mYs_IvklFWHqfBA4YTccpiG_0mLFlehAN0NWz-jqVtm8adkl-msrgVsf-2JgfN29dH6vYWuv9qz2pxI5riKxydvnHAsUi9tXAaH5kI_dtW0_3e5TXMjg7Iij9KGNeeyumgVMDQy4FmkfFO7gRGVb0yYkIg6mAWTxQWPzvjtod52z7TdU_LLHKkVpG2xPtQXJ0T4nm3w1pvcnyNLuqoRm0zVPEuKOmOGbA5YR19qgnNdu2T1nrPRPy4Oqwa9fx_2RkQWG48jN359HSaZiTIxr4bVe0D3fAGYbr2aRX2wx2XlnIJcm7eV58CFgRJe50R9zU4AetVnvAuOixeYqJN1dOARQtFh6fBhNOw-TfxORLjCs4qmefdWWGub2na21qVhlaTKy9DohJuAMP93yZFQ0kPrrqISe2Yitr0wST0v7n2p7PV39MwQ4bGVmLS3Oo27GWpy8726Vo-_NQP-m0UAadVLIKjgW3LtzsVaHv-utPsWb9wkUjL5n4ic";
-
-            given()
-                    .auth().oauth2(token)
-                    .get("https://passport.skillbox.ru/api/userinfo")
-                    .then()
-                    .log().all()
-                    .statusCode(200)
-                    .body("email", equalTo("dzmitryi.miadzvedzeu@gmail.com"));
-        }
-
-        @Test
-        public void testApplePageLogin() {
-            when().
-                    get("\n" +
-                            "https://appleid.apple.com/auth/authorize?client_id=skillbox.passport&redirect_uri=https%3A%2F%2Fpassport.skillbox.ru%2Flogin%2Fapple%2Fcallback&scope=name%20email&response_type=code&response_mode=form_post&state=IqCkRmyn9C6iDrUYdnGmt5aAkng9irlHViSEgEWX&nonce=7848ed76-54a3-4113-a3aa-428206f9df8a.IqCkRmyn9C6iDrUYdnGmt5aAkng9irlHViSEgEWX").
-                    then().
-                    statusCode(200);
-        }
-
-
-        @Test
-        public void testCurrentUserNotAuth() {
-            when().
-                    get("\n" +
-                            "https://go.skillbox.ru/api/v3/users/current/").
-                    then().
-                    log().all().
-                    body("data", equalTo("Учетные данные не были предоставлены.")).
-                    statusCode(401);
-        }
-
-        @Test
-        public void testCurrentUserAuth() {
-            Map<String, String> headers = new HashMap<>();
-            headers.put("X-Auth-Passport", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5YTczZmEyYi0yOTk4LTRlNjEtYWU5YS02N2NmMzdhZmQyZWIiLCJqdGkiOiIyMGQ3OGZmNTVlZjRiOTczYmMyM2ExMDI0ZTMwZmU4NjQ1YjBjYzBmZWJkMWJiZTk2YjJjZmJmMjUyN2NkODUzNzMzNGMzNGI4NzY3MDI4NSIsImlhdCI6MTcyNzMyODE1Mi43Mzg2NDcsIm5iZiI6MTcyNzMyODE1Mi43Mzg2NDksImV4cCI6MTcyNzM3MTM1Mi43MjIxOTYsInN1YiI6IjQwMDk4MzciLCJyb2xlcyI6W10sInJvbGVzX2J5X2NvbXBhbmllcyI6e30sImlzX2Jsb2NrZWQiOmZhbHNlLCJzY29wZXMiOlsib3BlbmlkIiwiZW1haWwiLCJwcm9maWxlIiwicm9sZXMiLCJwZXJtaXNzaW9ucyJdfQ.Fa5Vjvz-bVMLvx0bajYajnD7R9L2i16iPl3_Kco-woMaRMkISfH6AunxQebf5BFQ5aYZDyFB0MxYCQwr0AemxoVTm_IH4bNi1Jo_dcjly0Td5QF16XvQYRyU84qQXYZb2mq3agHLHzhifDY3_xf0azIXI4uQKPjWItFMlDBYP8C-3Jx-iBq1mYs_IvklFWHqfBA4YTccpiG_0mLFlehAN0NWz-jqVtm8adkl-msrgVsf-2JgfN29dH6vYWuv9qz2pxI5riKxydvnHAsUi9tXAaH5kI_dtW0_3e5TXMjg7Iij9KGNeeyumgVMDQy4FmkfFO7gRGVb0yYkIg6mAWTxQWPzvjtod52z7TdU_LLHKkVpG2xPtQXJ0T4nm3w1pvcnyNLuqoRm0zVPEuKOmOGbA5YR19qgnNdu2T1nrPRPy4Oqwa9fx_2RkQWG48jN359HSaZiTIxr4bVe0D3fAGYbr2aRX2wx2XlnIJcm7eV58CFgRJe50R9zU4AetVnvAuOixeYqJN1dOARQtFh6fBhNOw-TfxORLjCs4qmefdWWGub2na21qVhlaTKy9DohJuAMP93yZFQ0kPrrqISe2Yitr0wST0v7n2p7PV39MwQ4bGVmLS3Oo27GWpy8726Vo-_NQP-m0UAadVLIKjgW3LtzsVaHv-utPsWb9wkUjL5n4ic");
-            given().headers(headers).
-                    get("\n" +
-                            "https://go.skillbox.ru/api/v3/users/current/").
-                    then().
-                    log().all().
-                    body("first_name", equalTo("Дмитрий")).
-                    body("last_name", equalTo("Медведев")).
-                    statusCode(200);
-        }
-
-        @Test
-        public void testLanguagesSupport() {
-            when().
-                    get("\n" +
-                            "https://go.skillbox.ru/api/v3/languages/").
-                    then().
-                    log().all().
-                    body("name", hasItems("Ўзбекча", "Қазақша", "Русский", "English"));
-        }
-
-        @Test
-        public void testUserSettings() {
-            when().
-                    get("\n" +
-                            "https://go.skillbox.ru/api/v3/system/settings/").
-                    then().
-                    log().all().
-                    body("employment_course_id", equalTo("eb866d28-6439-435f-878f-391775b618a7")).
-                    body("onboarding_course_id", equalTo("6cb6e03d-d21c-448a-b491-52e683193fc9")).
-                    statusCode(200);
-        }
+    @Test
+    public void testVKLogin() {
+        when().
+                post(LoginRequest.VK_LOGIN_URL).
+                then().
+                log().all().
+                statusCode(200).
+                body(LoginRequest.getVKLoginErrorInfo()[0], equalTo(LoginRequest.getVKLoginErrorInfo()[1]));
     }
+
+    @Test
+    public void testUnLogin() {
+        given().headers(LoginRequest.getHeaders()).
+                when().
+                get(LoginRequest.NOT_LOGIN_URL).
+                then().
+                statusCode(401).
+                body(LoginRequest.notLoginResponse()[0], equalTo(LoginRequest.notLoginResponse()[1]));
+    }
+
+    @Test
+    public void testUserInfo() {
+        given()
+                .auth().oauth2(LoginRequest.BEARER_TOKEN)
+                .get(LoginRequest.VK_USER_INFO_URL)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(LoginRequest.userInfoResponse()[0], equalTo(LoginRequest.userInfoResponse()[1]));
+    }
+
+    @Test
+    public void testApplePageLogin() {
+        when().
+                get(LoginRequest.APPLE_LOGIN_URL).
+                then().
+                statusCode(200);
+    }
+
+
+    @Test
+    public void testCurrentUserNotAuth() {
+        when().
+                get(LoginRequest.NOT_AUTH_CURRENT_URL).
+                then().
+                log().all().
+                body(LoginRequest.notAuthCurrentResponse()[0], equalTo(LoginRequest.notAuthCurrentResponse()[1])).
+                statusCode(401);
+    }
+
+    @Test
+    public void testCurrentUserAuth() {
+        given().headers(LoginRequest.getHeadersAuth()).
+                get(LoginRequest.CURRENT_USER_AUTH_URL).
+                then().
+                log().all().
+                body(LoginRequest.authCurrentResponse()[0], equalTo(LoginRequest.authCurrentResponse()[1])).
+                body(LoginRequest.authCurrentResponse()[2], equalTo(LoginRequest.authCurrentResponse()[3])).
+                statusCode(200);
+    }
+
+    @Test
+    public void testLanguagesSupport() {
+        when().
+                get(LoginRequest.SUPPORT_LANGUAGE_URL).
+                then().
+                log().all().
+                body(LoginRequest.langSupportResponse()[0], hasItems(LoginRequest.langSupportResponse()[1], LoginRequest.langSupportResponse()[2], LoginRequest.langSupportResponse()[3], LoginRequest.langSupportResponse()[4]));
+    }
+
+    @Test
+    public void testUserSettings() {
+        when().
+                get(LoginRequest.CURRENT_SETTINGS_URL).
+                then().
+                log().all().
+                body(LoginRequest.currentSettingsResponse()[0], equalTo(LoginRequest.currentSettingsResponse()[1])).
+                body(LoginRequest.currentSettingsResponse()[2], equalTo(LoginRequest.currentSettingsResponse()[3])).
+                statusCode(200);
+    }
+}
--- src/test/java/com/webisite/dmitriymedvedev/apitests/models/LoginRequest.java
+++ src/test/java/com/webisite/dmitriymedvedev/apitests/models/LoginRequest.java
@@ -2,17 +2,64 @@ package com.webisite.dmitriymedvedev.apitests.models;
 
 import io.restassured.matcher.ResponseAwareMatcher;
 import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 import static com.google.common.base.Predicates.equalTo;
 
+public class LoginRequest {
 
-    public class LoginRequest {
-        public static final String VK_LOGIN_URL = "https://login.vk.com/?act=connect_user_info";
+    public static final String BEARER_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5YTczZmEyYi0yOTk4LTRlNjEtYWU5YS02N2NmMzdhZmQyZWIiLCJqdGkiOiIyMGQ3OGZmNTVlZjRiOTczYmMyM2ExMDI0ZTMwZmU4NjQ1YjBjYzBmZWJkMWJiZTk2YjJjZmJmMjUyN2NkODUzNzMzNGMzNGI4NzY3MDI4NSIsImlhdCI6MTcyNzMyODE1Mi43Mzg2NDcsIm5iZiI6MTcyNzMyODE1Mi43Mzg2NDksImV4cCI6MTcyNzM3MTM1Mi43MjIxOTYsInN1YiI6IjQwMDk4MzciLCJyb2xlcyI6W10sInJvbGVzX2J5X2NvbXBhbmllcyI6e30sImlzX2Jsb2NrZWQiOmZhbHNlLCJzY29wZXMiOlsib3BlbmlkIiwiZW1haWwiLCJwcm9maWxlIiwicm9sZXMiLCJwZXJtaXNzaW9ucyJdfQ.Fa5Vjvz-bVMLvx0bajYajnD7R9L2i16iPl3_Kco-woMaRMkISfH6AunxQebf5BFQ5aYZDyFB0MxYCQwr0AemxoVTm_IH4bNi1Jo_dcjly0Td5QF16XvQYRyU84qQXYZb2mq3agHLHzhifDY3_xf0azIXI4uQKPjWItFMlDBYP8C-3Jx-iBq1mYs_IvklFWHqfBA4YTccpiG_0mLFlehAN0NWz-jqVtm8adkl-msrgVsf-2JgfN29dH6vYWuv9qz2pxI5riKxydvnHAsUi9tXAaH5kI_dtW0_3e5TXMjg7Iij9KGNeeyumgVMDQy4FmkfFO7gRGVb0yYkIg6mAWTxQWPzvjtod52z7TdU_LLHKkVpG2xPtQXJ0T4nm3w1pvcnyNLuqoRm0zVPEuKOmOGbA5YR19qgnNdu2T1nrPRPy4Oqwa9fx_2RkQWG48jN359HSaZiTIxr4bVe0D3fAGYbr2aRX2wx2XlnIJcm7eV58CFgRJe50R9zU4AetVnvAuOixeYqJN1dOARQtFh6fBhNOw-TfxORLjCs4qmefdWWGub2na21qVhlaTKy9DohJuAMP93yZFQ0kPrrqISe2Yitr0wST0v7n2p7PV39MwQ4bGVmLS3Oo27GWpy8726Vo-_NQP-m0UAadVLIKjgW3LtzsVaHv-utPsWb9wkUjL5n4ic";
+    public static final String VK_LOGIN_URL = "https://login.vk.com/?act=connect_user_info";
+    public static final String VK_USER_INFO_URL = "https://passport.skillbox.ru/api/userinfo";
+    public static final String APPLE_LOGIN_URL = "https://passport.skillbox.ru/login/apple/";
+    public static final String NOT_LOGIN_URL = "https://passport.skillbox.ru/authapi/v1/user/";
+    public static final String NOT_AUTH_CURRENT_URL = "https://go.skillbox.ru/api/v3/users/current/";
+    public static final String CURRENT_USER_AUTH_URL = "https://go.skillbox.ru/api/v3/users/current";
+    public static final String SUPPORT_LANGUAGE_URL = "https://go.skillbox.ru/api/v3/languages/";
+    public static final String CURRENT_SETTINGS_URL = "https://go.skillbox.ru/api/v3/system/settings/";
 
-        public static String[] getVKLoginErrorInfo() {
-            return new String[] { "error_info", "Некарэктны URL" };
-        }
+    public static String[] getVKLoginErrorInfo() {
+        return new String[]{"error_info", "Некарэктны URL"};
     }
 
+    public static String[] userInfoResponse() {
+        return new String[]{"email", "dzmitryi.miadzvedzeu@gmail.com"};
+    }
+
+    public static String[] notLoginResponse() {
+        return new String[]{"message", "Unauthenticated."};
+    }
+
+    public static String[] notAuthCurrentResponse() {
+        return new String[]{"data", "Учетные данные не были предоставлены."};
+    }
+
+    public static String[] authCurrentResponse() {
+        return new String[]{"first_name", "Дмитрий", "last_name", "Медведев"};
+    }
+
+    public static String[] langSupportResponse() {
+        return new String[]{"name", "Ўзбекча", "Қазақша", "Русский", "English"};
+    }
+
+    public static String[] currentSettingsResponse() {
+        return new String[]{"employment_course_id", "eb866d28-6439-435f-878f-391775b618a7", "onboarding_course_id", "6cb6e03d-d21c-448a-b491-52e683193fc9"};
+    }
+
+    public static Map<String, String> getHeaders() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Accept", "application/json, text/plain, */*");
+        return headers;
+    }
+
+    public static Map<String, String> getHeadersAuth() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("X-Auth-Passport", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5YTczZmEyYi0yOTk4LTRlNjEtYWU5YS02N2NmMzdhZmQyZWIiLCJqdGkiOiIyMGQ3OGZmNTVlZjRiOTczYmMyM2ExMDI0ZTMwZmU4NjQ1YjBjYzBmZWJkMWJiZTk2YjJjZmJmMjUyN2NkODUzNzMzNGMzNGI4NzY3MDI4NSIsImlhdCI6MTcyNzMyODE1Mi43Mzg2NDcsIm5iZiI6MTcyNzMyODE1Mi43Mzg2NDksImV4cCI6MTcyNzM3MTM1Mi43MjIxOTYsInN1YiI6IjQwMDk4MzciLCJyb2xlcyI6W10sInJvbGVzX2J5X2NvbXBhbmllcyI6e30sImlzX2Jsb2NrZWQiOmZhbHNlLCJzY29wZXMiOlsib3BlbmlkIiwiZW1haWwiLCJwcm9maWxlIiwicm9sZXMiLCJwZXJtaXNzaW9ucyJdfQ.Fa5Vjvz-bVMLvx0bajYajnD7R9L2i16iPl3_Kco-woMaRMkISfH6AunxQebf5BFQ5aYZDyFB0MxYCQwr0AemxoVTm_IH4bNi1Jo_dcjly0Td5QF16XvQYRyU84qQXYZb2mq3agHLHzhifDY3_xf0azIXI4uQKPjWItFMlDBYP8C-3Jx-iBq1mYs_IvklFWHqfBA4YTccpiG_0mLFlehAN0NWz-jqVtm8adkl-msrgVsf-2JgfN29dH6vYWuv9qz2pxI5riKxydvnHAsUi9tXAaH5kI_dtW0_3e5TXMjg7Iij9KGNeeyumgVMDQy4FmkfFO7gRGVb0yYkIg6mAWTxQWPzvjtod52z7TdU_LLHKkVpG2xPtQXJ0T4nm3w1pvcnyNLuqoRm0zVPEuKOmOGbA5YR19qgnNdu2T1nrPRPy4Oqwa9fx_2RkQWG48jN359HSaZiTIxr4bVe0D3fAGYbr2aRX2wx2XlnIJcm7eV58CFgRJe50R9zU4AetVnvAuOixeYqJN1dOARQtFh6fBhNOw-TfxORLjCs4qmefdWWGub2na21qVhlaTKy9DohJuAMP93yZFQ0kPrrqISe2Yitr0wST0v7n2p7PV39MwQ4bGVmLS3Oo27GWpy8726Vo-_NQP-m0UAadVLIKjgW3LtzsVaHv-utPsWb9wkUjL5n4ic");
+        return headers;
+    }
+}
